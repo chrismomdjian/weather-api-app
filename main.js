d17cc411023c6018b3420234e1de65bdcbb40912
chrismomdjian <chrismomdjian@gmail.com>
2017-07-11 17:50:47 -0700

    added error messages

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ const weather_information = require("./weather.js");
 var user_city_input = process.argv.slice(2);
 var user_city_complete = user_city_input.join(" ");
 
-const city_name = user_city_complete;
-const country_code = "us";
+const city_name = user_city_complete.slice(0, -2).trim(); // Don't leave trailing whitespace at end of city name
+const country_code = user_city_complete.slice(-2); // get last two letters to represent country code
+console.log(city_name, country_code);
 
 weather_information.get(city_name, country_code);
--- weather.js
+++ weather.js
@@ -4,28 +4,37 @@ const http = require("http");
 const API_KEY = "example_key";
 
 function printMessage(city, country, degrees_fahrenheit) {
-  console.log(`The weather in ${city}, ${country} is ${degrees_fahrenheit}.`);
+  console.log(`The weather in ${city}, ${country} is ${degrees_fahrenheit}F.`);
 }
 
 function get(city_name, country_code) {
-  const request = http.get(`http://api.openweathermap.org/data/2.5/weather?q=${city_name},${country_code}&units=imperial&APPID=${API_KEY}`, (response) => {
-      var body = "";
+  try {
+    const request = http.get(`http://api.openweathermap.org/data/2.5/weather?q=${city_name},${country_code}&units=imperial&APPID=${API_KEY}`, (response) => {
+        var body = "";
 
-      response.on("data", (data) => {
-        body += data.toString();
+        response.on("data", (data) => {
+          body += data.toString();
+        });
+
+        response.on("end", () => {
+          var parsed = JSON.parse(body);
+          if (parsed.name != undefined) {
+            var temperature = parsed.main.temp;
+            printMessage(city_name, country_code, temperature);
+          } else {
+            console.log(`The location '${city_name}' was not found.`);
+            console.log(parsed.name);
+          }
+        });
       });
 
-      response.on("end", () => {
-        var parsed = JSON.parse(body);
-        var temperature = parsed.main.temp;
-        printMessage(city_name, country_code, temperature);
-      });
+
+    request.on("error", (error) => {
+      console.error(error.message);
     });
-
-
-  request.on("error", (error) => {
-    console.error(error.message);
-  });
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 module.exports.get = get; // Allow other files to require this one
