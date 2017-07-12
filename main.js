af32fceed10c5533595c989f1a0b624278adf5b3
chrismomdjian <chrismomdjian@gmail.com>
2017-07-11 17:03:13 -0700

    created weather.js file for http request connections and functions

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-const https = require("https");
-const http = require("http");
-
-const API_KEY = "example_key";
-var user_city_input = process.argv.slice(2);
-var user_city_complete = user_city_input.join(" ");
-
-const city_name = user_city_complete;
-const country_code = "us";
+const weather_information = require("./weather.js");
 
 // Example API call by city name and country code
 // api.openweathermap.org/data/2.5/weather?q={city name},{country code}
@@ -14,25 +6,10 @@ const country_code = "us";
 // By ZIP
 // api.openweathermap.org/data/2.5/weather?zip={zipcode},{country code}
 
-function printMessage(city, country, degrees_fahrenheit) {
-  console.log(`The weather in ${city}, ${country} is ${degrees_fahrenheit}.`);
-}
+var user_city_input = process.argv.slice(2);
+var user_city_complete = user_city_input.join(" ");
 
-const request = http.get(`http://api.openweathermap.org/data/2.5/weather?q=${city_name},${country_code}&units=imperial&APPID=${API_KEY}`, (response) => {
-    var body = "";
+const city_name = user_city_complete;
+const country_code = "us";
 
-    response.on("data", (data) => {
-      body += data.toString();
-    });
-
-    response.on("end", () => {
-      var parsed = JSON.parse(body);
-      var temperature = parsed.main.temp;
-      printMessage(city_name, country_code, temperature);
-    });
-  });
-
-
-request.on("error", (error) => {
-  console.error(error.message);
-});
+weather_information.get(city_name, country_code);
