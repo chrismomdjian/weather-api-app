7ddad4ba9748d22b330c2b8a4513f8591d86a72c
chrismomdjian <chrismomdjian@gmail.com>
2017-07-11 17:03:59 -0700

    created weather.js file for http request connections and functions

---
+++ weather.js
@@ -0,0 +1,31 @@
+const https = require("https");
+const http = require("http");
+
+const API_KEY = "example_key";
+
+function printMessage(city, country, degrees_fahrenheit) {
+  console.log(`The weather in ${city}, ${country} is ${degrees_fahrenheit}.`);
+}
+
+function get(city_name, country_code) {
+  const request = http.get(`http://api.openweathermap.org/data/2.5/weather?q=${city_name},${country_code}&units=imperial&APPID=${API_KEY}`, (response) => {
+      var body = "";
+
+      response.on("data", (data) => {
+        body += data.toString();
+      });
+
+      response.on("end", () => {
+        var parsed = JSON.parse(body);
+        var temperature = parsed.main.temp;
+        printMessage(city_name, country_code, temperature);
+      });
+    });
+
+
+  request.on("error", (error) => {
+    console.error(error.message);
+  });
+}
+
+module.exports.get = get; // Allow other files to require this one
